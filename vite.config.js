cd841e65c5d00b7c465039e35d2b76b2f455bbf9
Landon Stucki <landonstucki@gmail.com>
2025-11-29 15:18:04 -0700

    Vite update
    Built

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,9 @@ export default defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html")
+        main: resolve(__dirname, "src/index.html"),
+        conditions: resolve(__dirname, 'src/conditions.html'),
+        visitors: resolve(__dirname, 'src/visitor-centers.html')
       }
     }
   }
